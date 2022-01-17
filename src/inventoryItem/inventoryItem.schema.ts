d634fc4326acdf167ed9850192966029b40bc64a
Nandan V <nandanv2702@gmail.com>
2022-01-16 19:49:26 -0600

    feat: pre-save hook to verify product & warehouse

--- src/inventoryItem/inventoryItem.schema.ts
+++ src/inventoryItem/inventoryItem.schema.ts
@@ -1,20 +1,41 @@
-import { prop, Ref } from '@typegoose/typegoose'
-import { ObjectId } from 'mongoose';
+import { pre, prop, Ref } from '@typegoose/typegoose'
+import { Types } from 'mongoose';
 import Product from '../product/product.schema';
 import Warehouse from '../warehouse/warehouse.schema'
 import { Field, ObjectType } from 'type-graphql';
+import ProductService from '../product/product.service';
+import WarehouseService from '../warehouse/warehouse.service';
+import { ValidationError } from 'apollo-server-express';
+import { IsNotEmpty } from 'class-validator';
 
+const productDb = new ProductService()
+const warehouseDb = new WarehouseService()
+
+@pre<InventoryItem>('save', async function () {
+    const valid = await Promise.all([
+        await productDb.findById(this.Product!._id.toString()),
+        await warehouseDb.findById(this.Warehouse!._id.toString())
+    ])
+
+    const invalid = valid.filter((x) => x === null)
+
+    if (invalid.length > 0) {
+        throw new ValidationError('Product / Warehouse does not exist')
+    }
+})
 @ObjectType()
 export default class InventoryItem {
     @Field(() => String)
-    readonly _id: ObjectId;
+    readonly _id: Types.ObjectId;
 
     @Field(() => Product)
     @prop({ ref: 'Product', required: true })
+    @IsNotEmpty()
     public Product!: Ref<Product>
 
     @Field(() => Warehouse)
     @prop({ ref: 'Warehouse', required: true })
+    @IsNotEmpty()
     public Warehouse!: Ref<Warehouse>
 
     @Field(() => Number)
